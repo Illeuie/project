13f905430784575c97a4580742a901074c6f2fbd
Dimon <D_imo_N@bigmir.net>
2015-11-30 13:01:40 +0200

    first commit

---
+++ js/IE.js
@@ -0,0 +1,10 @@
+
+<script>
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
+document.createElement('hgroup');
+</script>
---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+	
+function changeBackground(cneck){
+    if(this.checked){
+        this.style.background=#4b9fa7; ;
+    }
+    else{
+        this.style.background = null;
+    	}
+	}
+}
